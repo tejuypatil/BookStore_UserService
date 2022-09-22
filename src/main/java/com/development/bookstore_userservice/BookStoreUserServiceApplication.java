08399e712432731824944c748fbc0e67408deb99
tejuypatil <tejuypatil@gmail.com>
2022-09-22 20:49:52 +0530

    Added Eureka client dependency

--- src/main/java/com/development/bookstore_userservice/BookStoreUserServiceApplication.java
+++ src/main/java/com/development/bookstore_userservice/BookStoreUserServiceApplication.java
@@ -2,8 +2,10 @@ package com.development.bookstore_userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookStoreUserServiceApplication {
 
     public static void main(String[] args) {
